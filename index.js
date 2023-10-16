510cf3afe4fd670989f428ed74824597bbe34747
Laura López <lauralopez166@gmail.com>
2023-10-16 12:38:02 +0200

    acabado

--- index.js
+++ index.js
@@ -4,34 +4,34 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const deleteAll = document.getElementById('delete');
     const task = document.getElementById('task');
 
+    add.addEventListener('click', () =>{
+        const textInto = text.value.trim();
+        if (textInto != "") {
+          newTask(textInto);
+          text.value = "";
+        }
+    })
+
+    text.addEventListener('keydown', function(event){
+        if(event.key === 'Enter'){
+            const textInto = text.value.trim();
+            if (textInto != "") {
+              newTask(textInto);
+              text.value = "";
+            } 
+        }
+        
+    })
+
     const newTask = (text) =>{
         const newTarea = document.createElement("li");
         newTarea.classList.add("task-item");
         newTarea.innerHTML=`<input type="checkbox" class="checkbox" id="checkbox"><label>${text}</label>`;
         task.appendChild(newTarea);
-        
-
-        
 
         deleteAll.addEventListener('click', () =>{  
             newTarea.remove();
         })
-
-        const tachado = document.getElementById('checkbox');
-        if (tachado === true){
-            text.style.textDecoration
-        }
     }
 
-    add.addEventListener('keydown', (event) =>{
-        if (event.key === 'enter'){
-            event.preventDefault();
-           add.click();
-        }
-        const textInto = text.value.trim();
-        if(textInto !=''){
-        newTask(textInto);
-        text.value="";
-    }
-    })
 })
